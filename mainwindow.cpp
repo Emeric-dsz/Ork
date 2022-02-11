f08068520290d26788981ce3b24f0e9d0b215d09
Emeric <emeric.dissez@gmail.com>
2022-02-11 14:20:55 +0000

    First button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,20 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    push_button2 = new QPushButton(this);
+    push_button2->setText("Click");
+    push_button2->setGeometry(QRect(QPoint(0, 0), QSize(100, 50)));
+    connect(push_button2, SIGNAL(clicked()), this, SLOT(on_pushButton_clicked()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->textEdit->setText("Let's Start !");
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QPushButton>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -15,7 +16,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QPushButton *push_button2;
 };
 #endif // MAINWINDOW_H
